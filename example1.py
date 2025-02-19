5cb4564bbff0c2acecf5520c10e53f4cc22dcf36
Matilda Sandra Akello <matildaakellosandra@gmail.com>
2025-02-19 11:12:48 +0300

    Rules governing variable names in Python

---
+++ example1.py
@@ -0,0 +1,11 @@
+#a name given to a partition through programming is called a variable ie a named memory space in the comp hard disk
+num1 = 65
+num2 = 100 #instruction written to a comp ie to find any next available memory and name it num2 and reserve it then store there 100
+print(num1)
+print(num1, num2)
+# RULES GOVERNING VARIABLE NAMES IN PYTHON
+#the name of a variable should never start with a numeral or numeric value eg 1num, 2num or special characters like .,@#? etc
+#compound variable names should not have spaces rather with _ or camel case or camel notation
+#avoid using capital letters to start your variable names
+#don't use special words as variables ig print, input etc
+#Variables in Python are case sensitive ie Num1 is not the same as num1
